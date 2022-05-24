b68a67d9cb571c81226f03a8a80cf91a15f79464
shufuge <106137829+shufuge@users.noreply.github.com>
2022-05-24 18:05:08 +0800

    gethub11

--- src/main/java/com/git/gtitest/Test1.java
+++ src/main/java/com/git/gtitest/Test1.java
@@ -9,5 +9,8 @@ public class Test1 {
         System.out.println("hello git5!");
         System.out.println("hello git6!");
         System.out.println("hello git7!");
+        System.out.println("hello git7!");
+        System.out.println("hello git8!");
+        
     }
 }
