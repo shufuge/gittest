046bbf77eee07d0cd2af302d0ee2d9973bb433e0
shu <shu@qq.com>
2022-05-24 16:57:44 +0800

    版本2

--- src/main/java/com/git/gtitest/Test1.java
+++ src/main/java/com/git/gtitest/Test1.java
@@ -3,5 +3,6 @@ package com.git.gtitest;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
